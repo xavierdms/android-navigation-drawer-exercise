d4f12b18907e0e64ab05cd7d3acd9e731e52faa9
xavierdms <id5@humboldt.edu>
2019-02-20 20:06:29 -0800

    hamburger icon

--- app/src/main/java/com/codepath/android/navigationdrawerexercise/activities/MainActivity.java
+++ app/src/main/java/com/codepath/android/navigationdrawerexercise/activities/MainActivity.java
@@ -1,6 +1,8 @@
 package com.codepath.android.navigationdrawerexercise.activities;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -8,6 +10,9 @@ import com.codepath.android.navigationdrawerexercise.R;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout mDrawer;
+    ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +24,25 @@ public class MainActivity extends AppCompatActivity {
         // Sets the Toolbar to act as the ActionBar for this Activity window.
         // Make sure the toolbar exists in the activity and is not null
         setSupportActionBar(toolbar);
+
+        // Find our drawer view
+        mDrawer = findViewById(R.id.drawer_layout);
+        mDrawerToggle = setupDrawerToggle(toolbar);
+
+        // Tie DrawerLayout events to the ActionBarToggle
+        mDrawer.addDrawerListener(mDrawerToggle);
+
+    }
+
+    private ActionBarDrawerToggle setupDrawerToggle(Toolbar toolbar) {
+        // NOTE: Make sure you pass in a valid toolbar reference.  ActionBarDrawToggle() does not require it
+        // and will not render the hamburger icon without it.
+        return new ActionBarDrawerToggle(this, mDrawer, toolbar, R.string.drawer_open,  R.string.drawer_close);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
     }
 }
